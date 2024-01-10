c98a0cb89bea4aad7ed6132eca8e67343e7d66ae
Chris Bibbs <cbibbs94@gmail.com>
2024-01-09 16:56:34 -0800

    Got consecutive views to work out

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -12,6 +12,7 @@ import { formatCurrency } from './utils';
 
 export async function fetchRevenue() {
   // Add noStore() here prevent the response from being cached.
+  
   // This is equivalent to in fetch(..., {cache: 'no-store'}).
 
   try {
@@ -24,6 +25,7 @@ export async function fetchRevenue() {
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
     // console.log('Data fetch completed after 3 seconds.');
+    // console.log(data);
 
     return data.rows;
   } catch (error) {
